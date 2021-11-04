b9743cf2376629d77d99e7ecc31569963fd3b31b
luke <luke44@gmail.com>
2021-11-04 15:34:32 +0000

    Notes class created. Can add, store and list notes

---
+++ js/note.js
@@ -0,0 +1,25 @@
+const AllNotes = require('./allnotes')
+
+allnotes = new AllNotes()
+
+class Note {
+
+  constructor(title, content) {
+    this.title = title
+    this.content = content
+    allnotes.getAll().push(this)
+  }
+  getTitle() {
+    return this.title
+  }
+  getContent() {
+    return this.content
+  }
+}
+
+
+module.exports = Note
+
+// Not sure how to create a class variable to store 
+// all created notes, so this is done with 
+// a new class and instance altogether, AllNotes
