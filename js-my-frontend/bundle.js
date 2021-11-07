720ec753b52a358fb8abdd2dbf99413ab879a013
luke <luke44@gmail.com>
2021-11-07 17:18:05 +0000

    Iterating across all stored notes. Now need to allow adding of a note

--- js-my-frontend/bundle.js
+++ js-my-frontend/bundle.js
@@ -17,11 +17,14 @@ var require_fetchNotes = __commonJS({
 // index.js
 var fetchNotes2 = require_fetchNotes();
 fetchNotes2((jsonData) => {
+  jsonData.forEach((element) => createElement(element));
+});
+createElement = (element) => {
   newDiv = document.createElement("div");
-  newContent = document.createTextNode(`${jsonData[0]["title"]}`);
+  newContent = document.createTextNode(`${element["title"]}: ${element["content"]}`);
   newDiv.className = "post";
   idNumber = document.querySelectorAll(".post").length + 1;
   newDiv.id = "post-" + idNumber;
   newDiv.appendChild(newContent);
   document.body.appendChild(newDiv);
-});
+};
--- js-my-frontend/index.js
+++ js-my-frontend/index.js
@@ -1,12 +1,18 @@
 const fetchNotes = require('./fetchNotes.js')
 
 fetchNotes((jsonData) => {
+  jsonData.forEach(element =>
+    createElement(element)
+  )
+})
+
+createElement = (element) => {
   newDiv = document.createElement("div");
-  newContent = document.createTextNode(`${jsonData[0]['title']}`);
+  // newContent.maxLength = "20"
+  newContent = document.createTextNode(`${element['title']}: ${element['content']}`);
   newDiv.className = 'post';
   idNumber = document.querySelectorAll('.post').length + 1
   newDiv.id = 'post-' + idNumber;
   newDiv.appendChild(newContent);
   document.body.appendChild(newDiv);
-
-})
+}
